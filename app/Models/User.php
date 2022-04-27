ed59de79f659dd2bfb6f7b6684a6f90f014814f8
“graaj1990” <graaj90@gmail.com>
2022-04-27 18:02:49 +0530

    auth middleware completed

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,11 +2,12 @@
 
 namespace App\Models;
 
+use Tymon\JWTAuth\Contracts\JWTSubject;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-use Laravel\Sanctum\HasApiTokens;
+use Laravel\Sanctum\HasApiTokens; 
 
 class User extends Authenticatable implements JWTSubject
 {
@@ -47,6 +48,6 @@ class User extends Authenticatable implements JWTSubject
     }
 
     public function getJWTCustomClaims(){
-        return $this->getKey();
+        return [];
     }
 }
